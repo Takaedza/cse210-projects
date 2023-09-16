5f4309de5bf1e7cc086ddf2651cded12b342f597
Takaedza <tmandie@live.com>
2023-09-17 01:16:43 +0200

    prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade  percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+
+        string gradeSign = "";
+    
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+
+            if (gradePercentage >= 95)
+            {
+                gradeSign = "+";
+            }
+
+            else
+            {
+                gradeSign = "-";
+            }
+
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+
+            if (gradePercentage >= 85)
+            {
+                gradeSign = "+";
+            }
+
+            else
+            {
+                gradeSign = "-";
+            }
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+
+            if (gradePercentage >= 75)
+            {
+                gradeSign = "+";
+            }
+
+            else
+            {
+                gradeSign = "-";
+            }
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine("Your Grade is:" + letter);
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you have passed the class.");
+        
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next semester.");
+        }
     }
 }
