4d6d5fa60b7131f9104243008c9b4e23ac24c19b
Takaedza <tmandie@live.com>
2023-10-27 02:39:16 +0200

    maths app

---
+++ prove/Develop03/Words.cs
@@ -0,0 +1,34 @@
+class Word
+{
+    private string text;
+
+    private bool hidden;
+
+    public Word(string text)
+    {
+        this.text = text;
+        this.hidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        if(this.hidden)
+        {
+            return "_________";
+        }
+        else
+        {
+            return this.text;
+        }
+    }
+
+    public bool isHidden()
+    {
+        return this.hidden;
+    }
+
+    public void Hide()
+    {
+        this.hidden = true;
+    }
+}
